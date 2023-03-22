429a5c0e25b906621743d219110f2e9ca2293569
RuDra KannoJiya <45325778+rudrak07@users.noreply.github.com>
2023-03-22 20:07:33 +0530

    Add files via upload

---
+++ PrePost.java
@@ -0,0 +1,18 @@
+//import java.util.Scanner;
+class PrePost
+{
+ public static void main(String args[])
+   {
+   int a=10;
+   System.out.println(a);
+   System.out.println("PrePost Incr...");
+   System.out.println(a++);
+   System.out.println(++a);
+   
+   System.out.println(a);
+   System.out.println("PrePost Decr...");
+   System.out.println(a--);
+   System.out.println(--a);
+    
+}
+}
