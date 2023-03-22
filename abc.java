cdec2ca9bb7a5712dc42908865bca51c18530031
RuDra KannoJiya <45325778+rudrak07@users.noreply.github.com>
2023-03-22 15:20:24 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+ class abc
+{
+
+public static void main(String ar[])
+  {
+  int l,b,area;
+  System.out.println("Enter the value length and base");
+  Scanner rec=new Scanner(System.in);
+  l=rec.nextInt();
+  b=rec.nextInt();
+  area=l*b;
+  System.out.println(area);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
